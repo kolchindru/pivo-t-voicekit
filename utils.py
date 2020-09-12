9df2da645fa7e5edf20d5aecc3509284fbd4924b
Michael Vasilkovsky <waytobehigh@gmail.com>
2020-09-13 02:03:33 +0300

    feat: add tts

--- utils.py
+++ utils.py
@@ -5,34 +5,55 @@ from dataclasses import dataclass
 from typing import List
 from word_to_number.extractor import NumberExtractor
 from telegram import Voice
-from voicekit.library_voicekit import stt_wav_to_string
+from voicekit.library_voicekit import stt_wav_to_string, tts_string_to_wav
+
+
+class TempFiles:
+    def __init__(self, *extensions):
+        self.extensions = extensions
+
+    def __enter__(self):
+        tmp_name = str(uuid.uuid4())
+        self._files = [tmp_name + ext for ext in self.extensions]
+        return self._files
+
+    def __exit__(self, *args):
+        for file in self._files:
+            os.remove(file)
+
+
+def _convert_audio(file_input, file_output):
+    process = subprocess.run(["ffmpeg", "-i", file_input, file_output])
+    if process.returncode != 0:
+        raise Exception("Can not convert audio!")
 
 
 def message_to_text(update, context, send_back_voice=True):
     if update.message.voice:
-        tmp_name = str(uuid.uuid4())
-        tmp_ogg = tmp_name + ".ogg"
-        tmp_wav = tmp_name + ".wav"
+        with TempFiles(".ogg", ".wav") as (tmp_ogg, tmp_wav):
+            voice_obj = update.message.voice
+            voice_file = Voice(voice_obj.file_id, voice_obj.file_unique_id, voice_obj.duration, bot=context.bot)
+            voice_file.get_file(timeout=100).download(tmp_ogg)
 
-        voice_obj = update.message.voice
-        voice_file = Voice(voice_obj.file_id, voice_obj.file_unique_id, voice_obj.duration, bot=context.bot)
-        voice_file.get_file(timeout=100).download(tmp_ogg)
+            _convert_audio(tmp_ogg, tmp_wav)
 
-        process = subprocess.run(["ffmpeg", "-i", tmp_ogg, tmp_wav])
-        if process.returncode != 0:
-            raise Exception("Can not convert .ogg to .wav!")
-
-        text = stt_wav_to_string(tmp_wav)
+            text = stt_wav_to_string(tmp_wav)
 
         if send_back_voice:
             context.bot.send_message(chat_id=update.effective_chat.id, text=f"\"{text}\"")
 
-        os.remove(tmp_ogg)
-        os.remove(tmp_wav)
-
         return text
 
     return update.message.text
+
+
+def send_text_as_voice(text, update, context):
+    with TempFiles(".ogg", ".wav") as (tmp_ogg, tmp_wav):
+        tts_string_to_wav(text, tmp_wav)
+        _convert_audio(tmp_wav, tmp_ogg)
+
+        with open(tmp_ogg, "rb") as voice_file:
+            context.bot.send_voice(chat_id=update.effective_chat.id, voice=voice_file)
 
 
 @dataclass
